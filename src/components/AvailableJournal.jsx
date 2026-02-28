50a23f1ae0bbbbbfa3e86b357e0612d22b6ebebe
getgitcode <mohammedismailpn@gmail.com>
2026-02-28 13:47:30 +0530

    api change

--- src/components/AvailableJournal.jsx
+++ src/components/AvailableJournal.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect, useRef } from "react";
 
 export default function AvailableJournal() {
 
-  const suggesturl = "https://lecture-playlist-mounted-compare.trycloudflare.com/suggest";
+  const suggesturl = "https://nextel-extras-suggests-mission.trycloudflare.com/suggest";
   const nav = useNavigate();
   const [rowsLimit, setRowsLimit] = useState(5);
   const [search, setSearch] = useState("");
