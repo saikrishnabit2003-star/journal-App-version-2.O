bc5b9456a2fef50888358c6af59674ecbfc3915a
getgitcode <mohammedismailpn@gmail.com>
2026-03-03 09:48:49 +0530

    api changed

--- src/components/AvailableJournal.jsx
+++ src/components/AvailableJournal.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect, useRef } from "react";
 
 export default function AvailableJournal() {
 
-  const suggesturl = "https://pastor-url-concept-refer.trycloudflare.com/suggest";
+  const suggesturl = "https://pricing-christmas-congratulations-johnson.trycloudflare.com/suggest";
   const nav = useNavigate();
   const [rowsLimit, setRowsLimit] = useState(5);
   const [search, setSearch] = useState("");
